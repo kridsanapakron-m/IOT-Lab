b5131c7b9c575d23fae82d8ddbf4a1670a0fd45b
kridsanapakron <141341438+kridsanapakron@users.noreply.github.com>
2025-07-20 13:27:59 +0700

    v1.1

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -8,6 +8,9 @@ import { students } from "../db/schema.js";
 
 
 const apiRouter = new Hono();
+apiRouter.get("/", (c) => {
+  return c.json({ message: "Student API" });
+});
 
 apiRouter.use(
   "/*",
@@ -18,10 +21,6 @@ apiRouter.use(
     },
   })
 );
-
-apiRouter.get("/", (c) => {
-  return c.json({ message: "Student API" });
-});
 
 apiRouter.get("/student", async (c) => {
   const allStudent = await drizzle.select().from(students);
