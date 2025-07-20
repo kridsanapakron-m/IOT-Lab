42d8f018215ede26087528d7ad0fc83c4330c4fb
kridsanapakron <141341438+kridsanapakron@users.noreply.github.com>
2025-07-20 10:38:21 +0700

    v1.1

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -1,14 +1,18 @@
 import { Hono } from "hono";
-import booksRouter from "./books.js";
 import { bearerAuth } from "hono/bearer-auth";
 import { env } from "hono/adapter";
+import drizzle from "../db/drizzle.js";
+import { students } from "../db/schema.js";
 
 const apiRouter = new Hono();
 
 apiRouter.get("/", (c) => {
   return c.json({ message: "Student API" });
 });
-
+apiRouter.get("/student", async (c) => {
+  const allStudent = await drizzle.select().from(students);
+  return c.json(allStudent);
+});
 apiRouter.use(
   "*",
   bearerAuth({
