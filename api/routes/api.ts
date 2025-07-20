05dff6e255522a1ff035bb7094f8121d2a9d0dc2
kridsanapakron <141341438+kridsanapakron@users.noreply.github.com>
2025-07-20 13:34:21 +0700

    v1.1

--- api/routes/api.ts
+++ api/routes/api.ts
@@ -3,10 +3,8 @@ import { bearerAuth } from "hono/bearer-auth";
 import { env } from "hono/adapter";
 import drizzle from "../db/drizzle.js";
 import { students } from "../db/schema.js";
-// import { zValidator } from "@hono/zod-validator";
-// import z from "zod";
-
-
+import { zValidator } from "@hono/zod-validator";
+import z from "zod";
 const apiRouter = new Hono();
 apiRouter.get("/", (c) => {
   return c.json({ message: "Student API" });
@@ -27,33 +25,33 @@ apiRouter.get("/student", async (c) => {
   return c.json(allStudent);
 });
 
-// apiRouter.post("/student",
-//   zValidator("json",z.object({
-//       firstName: z.string().min(1),
-//       lastName: z.string().min(1),
-//       studentId: z.string().min(1),
-//       birthDate: z.preprocess((arg) => {
-//         if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
-//         return arg;
-//       }, z.date()),
-//       gender: z.string().min(1),
-//     })
-//   ),
-//   async (c) => {
-//     const { firstName, lastName, studentId, birthDate, gender } = c.req.valid("json");
-//     const birthDateString = birthDate.toISOString().split('T')[0]; 
-//     const result = await drizzle
-//       .insert(students)
-//       .values({
-//         firstName,
-//         lastName,
-//         studentId,
-//         birthDate: birthDateString,
-//         gender,
-//       })
-//       .returning();
-//     return c.json({ success: true, book: result[0] }, 201);
-//   }
-// );
+apiRouter.post("/student",
+  zValidator("json",z.object({
+      firstName: z.string().min(1),
+      lastName: z.string().min(1),
+      studentId: z.string().min(1),
+      birthDate: z.preprocess((arg) => {
+        if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
+        return arg;
+      }, z.date()),
+      gender: z.string().min(1),
+    })
+  ),
+  async (c) => {
+    const { firstName, lastName, studentId, birthDate, gender } = c.req.valid("json");
+    const birthDateString = birthDate.toISOString().split('T')[0]; 
+    const result = await drizzle
+      .insert(students)
+      .values({
+        firstName,
+        lastName,
+        studentId,
+        birthDate: birthDateString,
+        gender,
+      })
+      .returning();
+    return c.json({ success: true, students: result[0] }, 201);
+  }
+);
 
 export default apiRouter;
